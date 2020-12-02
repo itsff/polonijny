2dd480c3fab940557c5c88447de611fd7beaff5b
Filip Frącz <filip@fracz.net>
2020-12-01 23:22:37 -0600

    Added user support
    - Added default ASP.NET Identity UI controllers
      and default views.
    - Added MongoDB Identity provider.
    - Added logon partial view and login link.

--- src/SlownikPolonijny.Web/Startup.cs
+++ src/SlownikPolonijny.Web/Startup.cs
@@ -5,10 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using AspNetCore.Identity.Mongo;
 using SlownikPolonijny.Dal;
 
 namespace SlownikPolonijny.Web
@@ -32,7 +36,36 @@ namespace SlownikPolonijny.Web
 
             services.AddSingleton<IRepository, MongoRepository>();
 
+            services.AddIdentityMongoDbProvider<Models.WebUser, Models.WebRole>(identityOptions =>
+            {
+                identityOptions.Password.RequiredLength = 6;
+                identityOptions.Password.RequireLowercase = false;
+                identityOptions.Password.RequireUppercase = false;
+                identityOptions.Password.RequireNonAlphanumeric = false;
+                identityOptions.Password.RequireDigit = false;
+                identityOptions.SignIn.RequireConfirmedAccount = true;
+                identityOptions.SignIn.RequireConfirmedEmail = true;
+            }, mongoIdentityOptions => {
+                mongoIdentityOptions.ConnectionString = Configuration.GetSection("Mongo")["ConnectionString"];
+            });
+  
+            services.AddAuthentication(o =>
+            {
+                o.DefaultScheme = IdentityConstants.ApplicationScheme;
+                o.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            });
+
+            services.AddIdentityCore<Models.WebUser>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdmin", policy => policy.RequireRole("@admin"));
+            });
+
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,11 +87,16 @@ namespace SlownikPolonijny.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
