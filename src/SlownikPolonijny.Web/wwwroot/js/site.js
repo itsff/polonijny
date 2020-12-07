e6afc44fce94dd6b0c64efd8b3559fb29cb98d57
Filip Frącz <filip@fracz.net>
2020-12-07 16:59:50 -0600

    Changed `var` in  JavaScript to `let` (#48)

--- src/SlownikPolonijny.Web/wwwroot/js/site.js
+++ src/SlownikPolonijny.Web/wwwroot/js/site.js
@@ -1,12 +1,12 @@
 ﻿
-var searchOptions = {
+let searchOptions = {
     url: function(phrase) { return "/szukaj/" + phrase; },
     requestDelay: 500,
     list: {
 		onChooseEvent: function() {
-            var name = $("#search").getSelectedItemData();
-            window.location.href = "/haslo/" + name;
-		}	
+                  let name = $("#search").getSelectedItemData();
+                  window.location.href = "/haslo/" + name;
+            }	
 	}
 };
 
