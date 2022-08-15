d73fdad586835308a631115b213e15f92427e59c
Filip Frącz <filip@fracz.net>
2022-08-15 01:33:39 -0500

    Bringing back HSTS redirect

--- src/SlownikPolonijny.Web/Startup.cs
+++ src/SlownikPolonijny.Web/Startup.cs
@@ -91,7 +91,7 @@ namespace SlownikPolonijny.Web
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStatusCodePagesWithReExecute("/Home/HandleError/{0}");
